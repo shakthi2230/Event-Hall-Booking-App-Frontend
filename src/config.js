303c9ef04baf1483efd8390d03f9409148c4209d
shakthi2230 <letsmail.sakthivel@gmail.com>
2025-01-09 14:59:04 +0530

    url changed

--- src/config.js
+++ src/config.js
@@ -1,8 +1,8 @@
-// const BASE_URL = 'http://localhost:8000';
+const BASE_URL = 'http://localhost:8000';
 
 // for live production
 
-const BASE_URL='https://event-hall-booking.vercel.app'
+// const BASE_URL='https://event-hall-booking.vercel.app'
 
 
 
